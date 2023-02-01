30766bb044adbe1cbb1bb3c16dbbc725bfa2f969
Promise Nwachukwu <promiseuchenwachukwu@gmail.com>
2023-01-31 19:55:03 -0500

    Added Infrastructure files

---
+++ main.tf
@@ -0,0 +1,59 @@
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = emirates-dnata-rg
+}
+
+resource "random_id" "log_analytics_workspace_name_suffix" {
+  byte_length = 8
+}
+
+resource "azurerm_log_analytics_workspace" "ednata" {
+  location            = var.log_analytics_workspace_location
+  name                = "${var.log_analytics_workspace_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = var.log_analytics_workspace_sku
+}
+
+resource "azurerm_log_analytics_solution" "ednata" {
+  location              = azurerm_log_analytics_workspace.ednata.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  solution_name         = "ContainerInsights"
+  workspace_name        = azurerm_log_analytics_workspace.ednata.name
+  workspace_resource_id = azurerm_log_analytics_workspace.ednata.id
+
+  plan {
+    product   = "OMSGallery/ContainerInsights"
+    publisher = "Microsoft"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "ednatak8s" {
+  location            = azurerm_resource_group.rg.location
+  name                = var.cluster_name
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.dns_prefix
+  tags                = {
+    Environment = "Development"
+  }
+
+  default_node_pool {
+    name       = "agentpool"
+    vm_size    = "Standard_D2_v2"
+    node_count = var.agent_count
+  }
+  linux_profile {
+    admin_username = "ubuntu"
+
+    ssh_key {
+      key_data = file(var.ssh_public_key)
+    }
+  }
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
+  }
+  service_principal {
+    client_id     = var.aks_service_principal_app_id
+    client_secret = var.aks_service_principal_client_secret
+  }
+}
